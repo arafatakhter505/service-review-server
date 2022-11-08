2ce771516625380e2e65601d1becb13dd5770ba0
arafatakhter505 <arafatakhterforhad@gmail.com>
2022-11-08 21:03:09 +0600

    get reviews by id

--- index.js
+++ index.js
@@ -68,6 +68,13 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/review/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await reviewsCollection.findOne(filter);
+      res.send(result);
+    });
+
     // update data
     app.put("/reviews/:id", async (req, res) => {
       const review = req.body;
