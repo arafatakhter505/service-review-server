de43e6b3d1a7ab461e00ae9d12d2414114e11ae6
arafatakhter505 <arafatakhterforhad@gmail.com>
2022-11-09 00:56:24 +0600

    new api add

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Tooth Fixers Server is running");
+});
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.grcofim.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
 
